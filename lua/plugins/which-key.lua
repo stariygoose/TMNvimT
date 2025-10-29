4580a86640e5b576e4d797dafa03af69195c2d35
stariygoose <samokhinmarks@gmail.com>
2025-10-29 12:49:20 +0100

    which keey

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -7,6 +7,15 @@ return {
       win = {
         border = "rounded",
       },
+      spec = {
+        { "<leader>f", group = "Find (Telescope)" },
+        { "<leader>c", group = "Code (LSP)" },
+        { "<leader>b", group = "Buffers" },
+        { "<leader>h", group = "Highlights" },
+        { "<leader>w", desc = "Save" },
+        { "<leader>q", desc = "Window quit" },
+        { "<leader>e", desc = "File Explorer" },
+      },
     },
     config = function(_, opts)
       local wk = require("which-key")
